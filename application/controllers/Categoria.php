3612574bedbccca3ffe08691aae4789186544507
Jefferson Martines <jefferson.martines@hotmail.com>
2023-04-15 16:26:44 -0300

    feat: aula 14/04

--- application/controllers/Categoria.php
+++ application/controllers/Categoria.php
@@ -1,20 +1,36 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+header('Access-Control-Allow-Origin: *');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+	header('Access-Control-Allow-Methods: POST, OPTIONS');
+	header('Access-Control-Allow-Headers: *');
+	exit;
+}
+
 class Categoria extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+	
+		$this->load->model("categoriamodel");
+	}
+	
 
 	public function adicionar() 
 	{
 		$this->load->library('form_validation');
 		
 		$this->form_validation->set_rules('nome', 'Nome', 'trim|required');
+		$this->form_validation->set_rules('status', 'Status', 'trim|required');
 		
 		$p = (object) $this->input->post();
 
 		if ($this->form_validation->run() == FALSE) {
 			$rps = array(
 				'status' => false,
-				'erros' => validation_erros()
+				'erros' => validation_errors()
 			);
 		} else {
 			$e = (object)[];
@@ -42,5 +58,10 @@ class Categoria extends CI_Controller {
 				);
 			}
 		}
+		echo json_encode($rps);
+	}
+
+	public function listar() {
+		
 	}
 }
